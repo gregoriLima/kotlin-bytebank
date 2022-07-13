27aedbf6cff7f8b004e94832621d28ec75c19ef5
Grégori Lima <gregorifl@gmail.com>
2022-07-13 03:10:45 -0300

    first commit

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,12 @@
       fun main(){
           println("testano")
+
+          var podeSerModificada = "modificavel"
+          val naoPodeSerModificada = "nao modificavel"
+
+          var asVariaveisDevemSerInicializadas : Int = 1
+
+          println("testando $podeSerModificada")
+          println(asVariaveisDevemSerInicializadas)
+
       }
